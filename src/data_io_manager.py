ea847fe224559eac9cfccd0811697783f792190f
Yiğitcan Dursun <ygtcan@Yigitcans-Air.home>
2024-08-14 11:34:46 +0300

    add postgres r/w methods

--- src/data_io_manager.py
+++ src/data_io_manager.py
@@ -3,6 +3,8 @@ from abc import ABC, abstractmethod
 import json
 import os
 import datetime
+from src.db_connections import PostgreSQLDB
+from sqlalchemy import text
 
 class BaseDataHandler(ABC):
 
@@ -93,3 +95,92 @@ class LocalDataHandler(BaseDataHandler):
                 raise IOError(f"Error writing file to {file_path}: {e}")
         else:
             raise ValueError(f"Unsupported file extension: {extension}")
+        
+class PostgresDataHandler(BaseDataHandler):
+    """Handles reading and writing data to a PostgreSQL database."""
+
+    def _init_(self):
+        self.postgres = PostgreSQLDB()
+        self.engine, self.session = self.postgres.connect()
+
+    def _get_postgres_type(self, series: pd.Series) -> str:
+        """
+        Maps a Pandas Series dtype to a PostgreSQL data type.
+
+        Args:
+            series (pd.Series): The Pandas Series to map.
+
+        Returns:
+            str: The PostgreSQL data type.
+        """
+        type_mapping = {
+            'object': 'TEXT',
+            'int64': 'BIGINT',
+            'int32': 'INT',
+            'float64': 'DOUBLE PRECISION',
+            'float32': 'FLOAT',
+            'datetime64[ns]': 'TIMESTAMP'
+        }
+
+        series_dtype = str(series.dtype)
+        return type_mapping.get(series_dtype, 'TEXT')
+
+
+    def _create_table(self, table_name: str, columns_info: str) -> None:
+        """
+        Creates a PostgreSQL table if it does not exist.
+
+        Args:
+            table_name (str): The name of the table.
+            columns_info (str): The columns and their data types.
+
+        Returns:
+            None
+        """
+        create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ({columns_info})"
+        try:
+            self.postgres.execute_query(text(create_table_query))
+            print(f"Table created successfully: {table_name}")
+        except Exception as e:
+            raise RuntimeError(f"Failed to create table {table_name}: {e}")
+
+    def write(self, data: pd.DataFrame, table_name: str) -> None:
+        """
+        Writes data to a PostgreSQL table.
+
+        Args:
+            data (pd.DataFrame): The DataFrame to write.
+            table_name (str): The name of the PostgreSQL table.
+
+        Returns:
+            None
+        """
+        try:
+            column_types = {column: self._get_postgres_type(data[column]) for column in data.columns}
+            columns_info = ', '.join([f"{col} {col_type}" for col, col_type in column_types.items()])
+            self._create_table(table_name, columns_info)
+            data.to_sql(table_name, con=self.engine, if_exists='append', index=False)
+            print(f"Successfully written DataFrame to PostgreSQL table: {table_name}")
+        except Exception as e:
+            raise RuntimeError(f"Failed to write DataFrame to PostgreSQL table {table_name}: {e}")
+        finally:
+            self.postgres.disconnect()
+
+    def read(self, table_name: str) -> pd.DataFrame:
+        """
+        Reads data from a PostgreSQL table.
+
+        Args:
+            table_name (str): The name of the table to read.
+
+        Returns:
+            pd.DataFrame: The DataFrame containing the read data.
+        """
+        try:
+            query = f"SELECT * FROM {table_name}"
+            result = self.postgres.execute_query(query)
+            data = result.fetchall()
+            columns = result.keys()
+            return pd.DataFrame(data, columns=columns)
+        except Exception as e:
+            raise RuntimeError(f"Failed to read data from PostgreSQL table {table_name}: {e}")
